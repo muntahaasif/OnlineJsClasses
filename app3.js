c16872d53a907a0457d42a521069535622b2f9cb
muntahaasif <muntahaasif82@gmail.com>
2025-06-29 07:47:22 -0700

    Add files via upload

---
+++ app3.js
@@ -0,0 +1,30 @@
+// VARIABLES FOR NUMBERS
+// 1. Declare a variable called age & assign to it your age. Show
+// your age in an alert box.
+// var age = "I am 19 years old"
+// alert(age);
+
+// 2. Declare & initialize a variable to keep track of how many
+// times a visitor has visited a web page. Show his/her
+// number of visits on your web page. For example: “You
+// have visited this site N times”.
+// var visit = "You have visited this site 14 times";
+// alert(visit);
+
+// 3. Declare a variable called birthYear & assign to it your
+// birth year. Show the following message in your browser:
+// var birthYear = 2006;
+// document.write("My birth year is " + birthYear + "<br>");
+// document.write("Data type of my declared data is number");
+
+// 4. A visitor visits an online clothing store
+// www.xyzClothing.com . Write a script to store in variables
+// the following information:
+// a. Visitor’s name
+// b. Product title
+// c. Quantity i.e. how many products a visitor wants to
+// order
+// Show the following message in your browser: “John
+// Doe ordered 5 T-shirt(s) on XYZ Clothing store”.
+// var info = "John Deo ordered 5  T-shirts(s) on XYZ Clothing store";
+// document.write(info);
