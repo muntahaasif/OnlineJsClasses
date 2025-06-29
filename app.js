8078edebac9c716c9277f0b2e5446d1f0443a409
muntahaasif <muntahaasif82@gmail.com>
2025-06-29 01:29:42 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+// // // ALERTS
+
+// // 1. Write a script to greet your website visitor using JS alert
+// // box.
+// alert("Welcome to our website");
+
+// 2. Write a script to display following message on your web
+// page:
+// alert("Error! Please enter a valid password.")
+
+// 3. Write a script to display following message on your web
+// page: (Hint : Use line break)
+//  alert("Welcome to JS land...\nHappy Coding!");
+
+// 4. Write a script to display following messages in sequence:
+// alert("Have a great day!");
+// alert("thankyou for visiting us")
+
+// 5. Generate the following message through browser’s
+// developer console:
+// console.log("Hello... i can run my JS through my web browser's console")
+
+//     6. Make use of alerts in your new/existing HTML & CSS
+// project.
+// alert("Welcome oue online classes");
+
+// // 7. write placement of <script></script> element in following section of your project in exercise 6.
+// (A):
+// <head>
+//     <script
+//     >Our JS code here
+//     </script>
+// </head>
+// (B):
+// <body>
+//     <script src="app.js"></script>
+// </body>
